b47ddfa5c62ff6f80115426454464a2dbacc7786
Nanai10a <Nanai10a@tutanota.com>
2021-01-18 15:35:46 +0900

    :construction: MainでDIする.

--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/Main.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/Main.kt
@@ -1,7 +1,13 @@
 package net.nanai10a.twomeat.cli
 
+import net.nanai10a.twomeat.cli.inits.interactorDI
+import net.nanai10a.twomeat.cli.inits.productionDI
+import net.nanai10a.twomeat.cli.utils.DIService
 import net.nanai10a.twomeat.cli.utils.Env
 
 fun main() {
     val env = Env()
+
+    val service = DIService().productionDI().interactorDI()
+
 }
