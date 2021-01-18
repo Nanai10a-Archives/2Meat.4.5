42627ab8ac9cc5951a280f341d850cba725fd2c4
Nanai10a <Nanai10a@tutanota.com>
2021-01-18 17:54:12 +0900

    :art: format.

--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/entities/User.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/entities/User.kt
@@ -1,6 +1,5 @@
 package net.nanai10a.twomeat.cli.entities
 
-import net.nanai10a.twomeat.cli.entities.History
 import java.util.*
 
 data class User(val id: UUID, val name: String, val discordId: String, val cli_history: History)
