d41c2c6abd81d683b2ec719daff83995bd0e4134
Nanai10a <Nanai10a@tutanota.com>
2021-01-19 02:36:43 +0900

    🎨format.

--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/DiscordIdGetView.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/DiscordIdGetView.kt
@@ -5,7 +5,8 @@ import net.dv8tion.jda.api.JDA
 import net.nanai10a.twomeat.cli.controllers.DiscordViewDestinationStore
 import net.nanai10a.twomeat.cli.usecases.SessionData
 
-class DiscordIdGetView(override val jda: JDA, private val destinationStore: DiscordViewDestinationStore) : IDiscordView<DiscordIdGetViewModel> {
+class DiscordIdGetView(override val jda: JDA, private val destinationStore: DiscordViewDestinationStore) :
+    IDiscordView<DiscordIdGetViewModel> {
     override fun invoke(sessionData: SessionData, model: DiscordIdGetViewModel) {
         val embed = EmbedBuilder()
             .addField("Id:get - Result", model.id, false)
