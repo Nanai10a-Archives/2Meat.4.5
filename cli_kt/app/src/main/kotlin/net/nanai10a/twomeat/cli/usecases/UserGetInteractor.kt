48fad6fa0f24a16a0d1e825b188dbc53f842f360
Nanai10a <Nanai10a@tutanota.com>
2021-01-18 15:30:57 +0900

    :construction: Interactor2種の実装.

---
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/usecases/UserGetInteractor.kt
@@ -0,0 +1,11 @@
+package net.nanai10a.twomeat.cli.usecases
+
+import net.nanai10a.twomeat.cli.db.gateways.IUserRepository
+import net.nanai10a.twomeat.cli.presenters.IUserGetPresenter
+
+class UserGetInteractor(private val presenter: IUserGetPresenter, private val repository: IUserRepository) :
+    IUserGetUsecase {
+    override fun handle(input: UserGetInputData) {
+        this.presenter.complete(UserGetOutputData(this.repository.load(input.discordId)))
+    }
+}
---
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/usecases/UserSaveInteractor.kt
@@ -0,0 +1,12 @@
+package net.nanai10a.twomeat.cli.usecases
+
+import net.nanai10a.twomeat.cli.db.gateways.IUserRepository
+import net.nanai10a.twomeat.cli.presenters.IUserSavePresenter
+
+class UserSaveInteractor(private val presenter: IUserSavePresenter, private val repository: IUserRepository) :
+    IUserSaveUsecase {
+    override fun handle(input: UserSaveInputData) {
+        repository.save(input.user)
+        this.presenter.complete(UserSaveOutputData())
+    }
+}
