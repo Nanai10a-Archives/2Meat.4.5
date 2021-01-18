7c09cc6ef2a92ff8c4101a241c894b52a745fa78
Nanai10a <Nanai10a@tutanota.com>
2021-01-19 00:00:20 +0900

    :art: format.

--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/DiscordUserGetView.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/DiscordUserGetView.kt
@@ -2,7 +2,6 @@ package net.nanai10a.twomeat.cli.presenters
 
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.entities.MessageEmbed
 import net.nanai10a.twomeat.cli.controllers.DiscordViewDestinationStore
 import net.nanai10a.twomeat.cli.usecases.SessionData
 
