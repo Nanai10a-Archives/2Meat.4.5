1e5bbb8f9704c9024722745a9d5ea13798d548a5
Nanai10a <Nanai10a@tutanota.com>
2021-01-18 23:55:06 +0900

    :construction: viewの要求が定義されたのでmodelも定義された.

--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/DiscordUserGetView.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/DiscordUserGetView.kt
@@ -1,14 +1,18 @@
 package net.nanai10a.twomeat.cli.presenters
 
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.nanai10a.twomeat.cli.controllers.DiscordViewDestinationStore
 import net.nanai10a.twomeat.cli.usecases.SessionData
 
 class DiscordUserGetView(override val jda: JDA, private val destinationStore: DiscordViewDestinationStore) :
     IDiscordView<DiscordUserGetViewModel> {
     override fun invoke(sessionData: SessionData, model: DiscordUserGetViewModel) {
-        TODO()
-        val string = ""
-        this.send(destinationStore.store[sessionData]!!, DiscordMessage(string, null, null))
+        val embed = EmbedBuilder()
+            .addField("User:get - Result", model.json, false)
+            .build()
+
+        this.send(destinationStore.store[sessionData]!!, DiscordMessage(null, null, embed))
     }
 }
--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/DiscordUserSaveView.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/DiscordUserSaveView.kt
@@ -1,5 +1,6 @@
 package net.nanai10a.twomeat.cli.presenters
 
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
 import net.nanai10a.twomeat.cli.controllers.DiscordViewDestinationStore
 import net.nanai10a.twomeat.cli.usecases.SessionData
@@ -7,8 +8,9 @@ import net.nanai10a.twomeat.cli.usecases.SessionData
 class DiscordUserSaveView(override val jda: JDA, private val destinationStore: DiscordViewDestinationStore) :
     IDiscordView<DiscordUserSaveViewModel> {
     override fun invoke(sessionData: SessionData, model: DiscordUserSaveViewModel) {
-        TODO()
-        val string = ""
-        this.send(destinationStore.store[sessionData]!!, DiscordMessage(string, null, null))
+        val embed = EmbedBuilder()
+            .addField("User:save - Result", "Success!", false).build()
+
+        this.send(destinationStore.store[sessionData]!!, DiscordMessage(null, null, embed))
     }
 }
