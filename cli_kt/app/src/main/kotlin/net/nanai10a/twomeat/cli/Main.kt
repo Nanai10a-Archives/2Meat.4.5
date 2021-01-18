5511b964227ee9a75fd36d800fc5259916a6b1d6
Nanai10a <Nanai10a@tutanota.com>
2021-01-19 01:27:06 +0900

    🚧main()はこんな感じ, 後はListener.

--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/Main.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/Main.kt
@@ -1,16 +1,28 @@
 package net.nanai10a.twomeat.cli
 
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.nanai10a.twomeat.cli.controllers.UserController
-import net.nanai10a.twomeat.cli.inits.controllerDI
-import net.nanai10a.twomeat.cli.inits.interactorDI
-import net.nanai10a.twomeat.cli.inits.presenterDI
+import net.nanai10a.twomeat.cli.inits.productionDI
+import net.nanai10a.twomeat.cli.presenters.DiscordUserGetEventTransmissioner
+import net.nanai10a.twomeat.cli.presenters.DiscordUserSaveEventTransmissioner
 import net.nanai10a.twomeat.cli.utils.Env
 import net.nanai10a.twomeat.cli.utils.ServiceProvider
 
 fun main() {
     val env = Env()
 
-    val service = ServiceProvider(env).presenterDI().interactorDI().controllerDI()
+    val userGetTransmissioner = DiscordUserGetEventTransmissioner()
+    val userSaveTransmissioner = DiscordUserSaveEventTransmissioner()
+
+    val jda = JDABuilder.createLight(env.discordToken).build()
+    jda.awaitReady()
+
+    val service = ServiceProvider(env).productionDI(jda, userSaveTransmissioner, userGetTransmissioner)
+
+    val controller = service.create(UserController::class.java)
 
     val controller = service.create(UserController::class.java) as UserController
 
