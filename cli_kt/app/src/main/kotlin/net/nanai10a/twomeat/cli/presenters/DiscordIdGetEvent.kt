0f179cbe2b84b035a0d55f186216dd51acca3e6b
Nanai10a <Nanai10a@tutanota.com>
2021-01-19 02:31:00 +0900

    🚧Presenter/Viewの実装done!

---
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/DiscordIdGetEvent.kt
@@ -0,0 +1,5 @@
+package net.nanai10a.twomeat.cli.presenters
+
+import net.nanai10a.twomeat.cli.usecases.SessionData
+
+data class DiscordIdGetEvent(val sessionData: SessionData, val model: DiscordIdGetViewModel)
