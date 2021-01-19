530c2dac55b4f5afcee5b9c0ca99ed720e112d16
Nanai10a <Nanai10a@tutanota.com>
2021-01-19 11:35:32 +0900

    🎨format.

--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/none/DiscordNoneView.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/presenters/none/DiscordNoneView.kt
@@ -6,7 +6,8 @@ import net.nanai10a.twomeat.cli.presenters.DiscordMessage
 import net.nanai10a.twomeat.cli.presenters.IDiscordView
 import net.nanai10a.twomeat.cli.usecases.SessionData
 
-class DiscordNoneView(override val jda: JDA, private val destinationStore: DiscordViewDestinationStore) : IDiscordView<DiscordNoneViewModel> {
+class DiscordNoneView(override val jda: JDA, private val destinationStore: DiscordViewDestinationStore) :
+    IDiscordView<DiscordNoneViewModel> {
     override fun invoke(sessionData: SessionData, model: DiscordNoneViewModel) =
         this.send(destinationStore.store[sessionData]!!, DiscordMessage(charSequence = model.rawString))
 }
