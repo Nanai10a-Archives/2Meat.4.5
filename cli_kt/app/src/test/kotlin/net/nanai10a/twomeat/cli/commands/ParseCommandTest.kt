bab7a2db2c84924c30d71d385a5c88499bd92f6d
Nanai10a <Nanai10a@tutanota.com>
2021-01-19 13:56:09 +0900

    ✅こんなもんか.

--- cli_kt/app/src/test/kotlin/net/nanai10a/twomeat/cli/commands/ParseCommandTest.kt
+++ cli_kt/app/src/test/kotlin/net/nanai10a/twomeat/cli/commands/ParseCommandTest.kt
@@ -10,5 +10,52 @@ internal class ParseCommandTest {
             listOf("A", "falsis,", "devatio", "magnum", "eleates."),
             func("A falsis, devatio magnum eleates.")
         )
+
+        assertEquals(
+            listOf("When", "the", "ferengi", "resists", "for", "nowhere,", "all", "ships", "gather", "post-apocalyptic,", "clear", "nanomachines."),
+            func("When    the    ferengi resists for      nowhere,    all ships gather   post-apocalyptic, clear nanomachines.")
+        )
+
+        assertEquals(
+            listOf("Happiness", "happens", "when", "you", "need", "extend", "so", "agreeable", "that", "whatsoever you are", "contacting", "is", "your", "vision."),
+            func("""Happiness happens when you need extend so agreeable that "whatsoever you are" contacting is your vision.""")
+        )
+
+        assertEquals(
+            listOf("a", "b", "c", "d"),
+            func("a　b\tc d")
+        )
+
+        assertEquals(
+            listOf("a", "b"),
+            func("a　\t\n\rb")
+        )
+
+        assertEquals(
+            listOf("a b"),
+            func("a\\ b")
+        )
+
+        assertEquals(
+            listOf(""),
+            func(" ")
+        )
+
+        assertEquals(
+            listOf("hey"),
+            func("""
+                 |hey
+             """.trimMargin("|"))
+        )
+
+        assertEquals(
+            listOf("hoge??", "　", "fuga??"),
+            func("""
+                 |hoge??
+                 |\　
+                 |　　　
+                 |fuga??
+             """.trimMargin("|"))
+        )
     }
 }
