1b76e538a6137045ee03a6703fb23c82c03a62e1
Nanai10a <Nanai10a@tutanota.com>
2021-01-19 11:25:30 +0900

    🎨format.

--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/commands/impl/PingPongCommand.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/commands/impl/PingPongCommand.kt
@@ -2,7 +2,6 @@ package net.nanai10a.twomeat.cli.commands.impl
 
 import net.nanai10a.twomeat.cli.commands.CommandFunction
 import net.nanai10a.twomeat.cli.commands.CommandFunctionInfo
-import net.nanai10a.twomeat.cli.controllers.UserController
 import net.nanai10a.twomeat.cli.usecases.SessionData
 
 class PingPongCommand : CommandFunction {
