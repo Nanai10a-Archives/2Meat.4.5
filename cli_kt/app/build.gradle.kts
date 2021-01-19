be3b096fe61bd08fd5befba09edb418e87092997
Nanai10a <Nanai10a@tutanota.com>
2021-01-19 13:34:11 +0900

    ➕jupiter使おうな.

--- cli_kt/app/build.gradle.kts
+++ cli_kt/app/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.google.guava:guava:29.0-jre")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 
