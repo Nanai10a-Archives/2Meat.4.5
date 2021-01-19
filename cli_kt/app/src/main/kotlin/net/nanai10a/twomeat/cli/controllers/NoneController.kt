c370b3162aff9b112d82a5bd059ecb4f60273d89
Nanai10a <Nanai10a@tutanota.com>
2021-01-19 11:50:04 +0900

    💩多分これが真理, やる気は失せた

--- cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/controllers/NoneController.kt
+++ cli_kt/app/src/main/kotlin/net/nanai10a/twomeat/cli/controllers/NoneController.kt
@@ -7,6 +7,8 @@ import net.nanai10a.twomeat.cli.usecases.SessionData
 
 class NoneController(
     private val presenter: DiscordNonePresenter
+    // FIXME: これ, CommandごとにUsecaseを定義してInteractorで動作を実装すべきだと思うんですよ, Entitiesに依存しないUsecaseって意味わからないけど
+    //        そう考えるとCommandごとにUsecaseが発生して動作を定義しなければなりません(Useのcasesだから当たり前なんですが...<-
 ) {
     fun send(sessionData: SessionData, args: List<String>) {
         val message = DiscordMessage(TODO())
